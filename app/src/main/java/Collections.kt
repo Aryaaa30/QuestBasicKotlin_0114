b1cc65ce31148db96a224ceb93411bdc173de7ee
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-10-13 13:13:55 +0700

    List yang read-only (immutable), tetapi isinya berasal dari list shape yang mutable

--- app/src/main/java/Collections.kt
+++ app/src/main/java/Collections.kt
@@ -27,4 +27,8 @@ fun ContohList(){
     // Mengubah data di dalam List Mutable
     shape[1] = "Oval"
     println(shape)
+
+    // List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
 }
