7a5a42b8e76b1401fc305c35a2ccd85c0f48595a
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-10-13 14:00:38 +0700

    Membuat Variabel neverNull yang berarti ia tidak dapat menyimpan nilai null

--- app/src/main/java/NullSafety.kt
+++ app/src/main/java/NullSafety.kt
@@ -1,5 +1,9 @@
 package com.example.test
 
 fun nullSafety(){
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
 
+    // Throws a compiler error
+    // neverNull = null
 }
