36f2a060fd8dd1a0637eadff9a516ff2a608bfe0
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-10-13 13:16:52 +0700

    Membuat fungsi set dengan nama ContohSet

--- app/src/main/java/Collections.kt
+++ app/src/main/java/Collections.kt
@@ -32,3 +32,12 @@ fun ContohList(){
     val shapesLocked: List<String> = shape
     println(shapesLocked)
 }
+
+// Set adalah kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi)
+// Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Set Read-Only menggunakan setOf
+
+fun ContohSet(){
+
+}
